8374bd112ba7a5b388cf0773c123e7a13e9f476e
tetraspace <holomanga@gmail.com>
2026-03-07 17:46:40 +0000

    migrate some homepage sections, add my lil 1 page projects

--- src/cra-project/components/HomePageSections/HomePageSections.tsx
+++ src/cra-project/components/HomePageSections/HomePageSections.tsx
@@ -1,12 +1,6 @@
 import { ContactMeSection } from "./ContactMeSection";
 import { FictionWritingSection } from "./FictionWritingSection";
-import { InvisibleMessageSection } from "./InvisibleMessageSection";
-import { ShillsListSection } from "./ShillsListSection";
-import { ThingsICareAboutSection } from "./ThingsICareAboutSection";
 export {
   ContactMeSection,
   FictionWritingSection,
-  InvisibleMessageSection,
-  ShillsListSection,
-  ThingsICareAboutSection,
 };
